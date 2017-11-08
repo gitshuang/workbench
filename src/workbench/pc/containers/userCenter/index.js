969435bdb2d677e169094d791c44403e5d1f1fc4
melvingod <melvingod@gmail.com>
2017-11-08 14:45:23 +0800

    个人中心添加tab

--- src/workbench/pc/containers/userCenter/index.js
+++ src/workbench/pc/containers/userCenter/index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { mapStateToProps } from '@u';
-import { wrap, outerContainer, active, imgUser, imgInner, userInfo, loginOut } from './style.css';
+import { wrap, outerContainer, active, imgUser, imgInner, userInfo, loginOut, tabContent } from './style.css';
 import homeActions from 'store/root/home/actions';
 import rootActions from 'store/root/actions';
+
 import Button from 'bee-button';
+import Tabs, { TabPane } from 'bee-tabs';
+import Icon from 'components/icon'
 
 const {
   getUserInfo,
@@ -62,7 +65,36 @@ class UserInfoContainer extends Component {
           </ul>
         </div>
         <div className={loginOut}>
-          <button colors="primary" onclick={ this.handleClick }>注销</button>
+          <Button colors="primary" size="sm" onClick={ this.handleClick }>注销</Button>
+        </div>
+
+        <div className={"um-content" + ` ${tabContent}`}>
+
+          <Tabs
+            defaultActiveKey="1"
+            onChange={this.callback}
+            tabBarStyle="upborder"
+            className="demo-tabs"
+          >
+            <TabPane tab='日常' key="1">
+              日常
+            </TabPane>
+            <TabPane tab='职业发展' key="2">
+              职业发展
+
+            </TabPane>
+            <TabPane tab='培训' key="4">
+              培训内容
+
+            </TabPane>
+            <TabPane tab='时间线' key="3">
+              时间线
+
+            </TabPane>
+
+          </Tabs>
+
+
         </div>
       </div>
     );
