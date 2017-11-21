48e6662050bae635a679b868870de3cb9aa1ebfe
zhengxingcheng <xingcheng.zheng@gmail.com>
2017-11-21 18:32:09 +0800

    先关掉消息部分功能
    * 关掉消息部分功能
    * 临时添加tinper-bee全部组件的css，后续删除

--- src/workbench/pc/pages/index.js
+++ src/workbench/pc/pages/index.js
@@ -72,7 +72,7 @@ class Root extends Component {
         requestSuccess();
       }
     });
-    timer(getMessage, 30000);
+    // timer(getMessage, 30000);
     regMessageTypeHandler.call(this);
     initMessage();
   }
