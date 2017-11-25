869e9ff56c963c2128096be86da4486fdf832788
xingychb <xingychb@yonyou.com>
2017-11-25 16:59:24 +0800

    pin  和 moveTogROUP

---
+++ src/workbench/pc/components/moveToGroup/index.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'bee-button';
+/*  style */
+import {
+  container,
+  title,
+  pd,
+  borderBox,
+  footer,
+  selectedli
+} from './style.css';
+
+
+class MoveToGroup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      newGroupName : '',      // 新分组名
+      isGroup : false,        // 是否是打开新的分组
+      way:"",
+      selectId:"",
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  setNewGroupName =(e) => {
+    this.setState({
+      newGroupName: e.target.value
+    })
+  }
+  // 子集文件夹的点击事件
+  handleClick =(item, index, list, key)=>{
+    let way = item.widgetName +"/"+ list.widgetName;
+    this.setState({
+      way: way,
+      selectId: list.widgetId
+    });
+  }
+  // menu一级菜单的点击事件
+  handleTitleClick = (item,index) => {
+    this.setState({
+      way: item.widgetName,
+      selectId: item.widgetId
+    });
+  }
+
+  render() {
+    let _this = this;
+    let content;
+    const {data,isGroup,addGroup,confirmFn,cancelFn,addNewGroup,groupCancelFn } = this.props;
+    if (isGroup){
+      content =
+        <div className= {`${pd} ${container}`}>
+          <div className={borderBox}>
+            <input type="text" value={this.state.newGroupName} onChange={ this.setNewGroupName }/>
+          </div>
+          <div className={footer + " um-box-justify"}>
+            <Button colors="danger" disabled={!this.state.newGroupName} onClick={addNewGroup}>添加到新分组</Button>
+            <Button onClick={groupCancelFn}>取消</Button>
+          </div>
+        </div>
+    }else{
+      content =
+        <div className= {container}>
+          <div className={title}>
+            添加到：{this.state.way}
+          </div>
+          <div className={borderBox}>
+            <ul>
+              {
+                data.map( (item, index) => {
+                  const classname = item.widgetId == this.state.selectId ? selectedli : ""
+                  return (
+                    <li key={index}>
+                      <p
+                        onClick={ ()=>{this.handleTitleClick(item,index)} }
+                        className={ classname}
+                      >
+                        {item.widgetName}
+                      </p>
+                      {
+                        item.children && item.children.map((list, key) => {
+                          const classname2 = list.widgetId == this.state.selectId ? selectedli : ""
+                          return (
+                            <div
+                              key = {key}
+                              className = { classname2 }
+                              onClick={ ()=>{this.handleClick(item, index, list, key)} }
+                            >
+                              {list.widgetName}
+                            </div>
+                          )
+                        })
+                      }
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          <div className={footer + " um-box-justify"}>
+            <div>
+              <Button onClick={addGroup}>添加分组</Button>
+            </div>
+            <div>
+              <Button colors="danger" disabled={!this.state.way} style={{marginRight:"5px"}} onClick={ confirmFn }>确定</Button>
+              <Button onClick={cancelFn}>取消</Button>
+            </div>
+          </div>
+        </div>
+    }
+    return content;
+  }
+}
+
+export default MoveToGroup;
--- src/workbench/pc/containers/pin/index.js
+++ src/workbench/pc/containers/pin/index.js
@@ -7,26 +7,18 @@ import { mapStateToProps } from '@u';
 import rootActions from 'store/root/actions';
 import workActions from 'store/root/work/actions';
 /*  comp */
+import MoveToGroup from 'components/moveToGroup';
 import Button from 'bee-button';
-import Menu, { SubMenu } from 'bee-menus';
-import Icon from 'components/icon';
 /*  style */
 import {
   pin,
   header,
-  container,
-  title,
-  pd,
-  borderBox,
-  footer,
-  selectedli
 } from './style.css';
 
 const {requestStart, requestSuccess, requestError} = rootActions;
 const { pinDisplayNone, setPinAdd, setAddGroup, getPinGroup } = workActions;
 
 @connect(mapStateToProps(
-  'pinType',
   'pinDisplay',
   {
     "namespace":"work"
@@ -49,12 +41,8 @@ class Pin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      way: '',
-      newGroupName : '',
-      isGroup : false,
-      currentMenu : 1,
       menuData: [],
-      selectedClass:false
+      isGroup: false
     }
   }
 
@@ -79,16 +67,16 @@ class Pin extends Component {
     }
   }
 
-  setReset = () => {
-
-  }
-
   addGroup = () => {
     this.setState({
       isGroup: true
     });
   }
-
+  groupCancelFn =() => {
+    this.setState({
+      isGroup: false,
+    });
+  }
   confirmFn = () => {
     const { requestError, setPinAdd } = this.props;
     setPinAdd().then(({ error, payload }) => {
@@ -101,160 +89,55 @@ class Pin extends Component {
 
   cancelFn = () => {
     const { pinDisplayNone } = this.props;
-    this.setState({
-      way: "",
-      menuData: this.cancelSelect()
-    });
     pinDisplayNone();
   }
   /*  menu  方法汇总   */
-  // 子集文件夹的点击事件
-  handleClick =(item, index, list, key)=>{
-      let way = item.name +"/"+ list.name;
-      this.setState({
-        way: way,
-        menuData: this.cancelSelect(index,key)
-      });
-  }
-  // menu一级菜单的点击事件
-  handleTitleClick = (item,index) => {
-    this.setState({
-      way: item.name,
-      menuData: this.cancelSelect(index)
-    });
-  }
-  //  统一的返回加工或点击之后的menuData数据
-  cancelSelect = (index,key) => {
-    let menuData = this.state.menuData;
-    menuData.forEach((v,i)=>{
-      v.checked = false;
-      v.widgeList && v.widgeList.forEach((w,k)=>{
-        w.checked = false;
-      })
-    });
-    //if(arguments.length == 0){
-    if( index == undefined && key == undefined){
-      return menuData;
-    }
-    if(key == undefined){
-      menuData[index].checked = true;
-      return menuData;
-    }
-    menuData[index].widgeList[key].checked = true;
-    return menuData;
-  }
 
   /*  下三个方法为  添加新组  method  */
-  addNewGroup =() => {
+  addNewGroup =(name,id) => {
     const { setAddGroup, requestError } = this.props;
+    let newGroup = {
+      id : id,
+      name : name,
+    };
     setAddGroup().then( ({ error, payload }) => {
       if (error) {
         requestError(payload);
       }
-      let newGroup = {
-        id : "2222",
-        name : this.state.newGroupName,
-      };
       let menuData = this.state.menuData;
       menuData.push(newGroup);
-
       this.setState({
         menuData: menuData,
       });
-      this.groupCancelFn();
     });
-  }
-  groupCancelFn =() => {
-    this.setState({
-      isGroup: false,
-      newGroupName: ""
-    });
-  }
-  setNewGroupName =(e) => {
-    this.setState({
-      newGroupName: e.target.value
-    })
-  }
-
-  renderContent = () => {
-    let _this = this;
-    let content;
-    let data = this.state.menuData;
-    if (this.state.isGroup){
-      content =
-        <div className= {`${pd} ${container}`}>
-          <div className={borderBox}>
-            <input type="text" value={this.state.newGroupName} onChange={ this.setNewGroupName }/>
-          </div>
-          <div className={footer + " um-box-justify"}>
-            <Button colors="danger" disabled={!this.state.newGroupName} onClick={this.addNewGroup}>添加到新分组</Button>
-            <Button onClick={this.groupCancelFn}>取消</Button>
-          </div>
-        </div>
-    }else{
-      content =
-        <div className= {container}>
-          <div className={title}>
-            添加到：{this.state.way}
-          </div>
-          <div className={borderBox}>
-            <ul>
-              {
-                data.map( (item, index) => {
-                  return (
-                    <li key={index}>
-                      <p
-                        onClick={ () =>{this.handleTitleClick(item,index)} }
-                        className={ item.checked ? selectedli : "" }
-                      >
-                        {item.name}
-                      </p>
-                      {
-                        item.widgeList && item.widgeList.map((list, key) => {
-                          return (
-                            <div
-                              key = {key}
-                              className = { list.checked ? selectedli : "" }
-                              onClick={() =>{this.handleClick(item, index, list, key)}}
-                            >
-                              {list.name}
-                            </div>
-                          )
-                        })
-                      }
-                    </li>
-                  )
-                })
-              }
-            </ul>
-          </div>
-          <div className={footer + " um-box-justify"}>
-            <div>
-              <Button onClick={this.addGroup}>添加分组</Button>
-            </div>
-            <div>
-              <Button colors="danger" disabled={!this.state.way} style={{marginRight:"5px"}} onClick={ this.confirmFn }>确定</Button>
-              <Button onClick={this.cancelFn}>取消</Button>
-            </div>
-          </div>
-        </div>
-    }
-    return content;
   }
 
   render() {
-    const { pinType, pinDisplay } = this.props;
-    return (
-      <div className={ pin + ' um-css3-hc'} style={{ display: pinDisplay ? 'block' : 'none' }} >
-        <div className={header +" um-box"}>
-          <div>
-            <img src="../" alt="" />
+    const { pinDisplay,classname } = this.props;
+    if(pinDisplay){
+      return (
+        <div className={`${pin} um-box-center`} >
+          <div className={header +" um-box"}>
+            <div>
+              <img src="../" alt="" />
+            </div>
+            <p>工作页名称</p>
           </div>
-          <p>工作页名称</p>
+
+          <MoveToGroup
+            data={this.state.menuData}
+            isGroup={this.state.isGroup}
+            addGroup={this.addGroup}
+            confirmFn={this.confirmFn}
+            cancelFn={this.cancelFn}
+            addNewGroup={this.addNewGroup }
+            groupCancelFn={this.groupCancelFn}
+          />
         </div>
-        {this.renderContent()}
-      </div>
-    );
+      );
+    }else{
+      return null
+    }
   }
 }
 
