e757e64cc1d87a35e9c645dc7572357158810020
yangchch6 <ycc54140127@163.com>
2017-12-04 10:54:56 +0800

    首页编辑header

---
+++ src/workbench/pc/pages/manage/headerPage.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { mapStateToProps } from '@u';
+import homeActions from 'store/root/home/actions';
+import Header from 'containers/header';
+import Navbar from 'components/scrollNav';
+import { logoImg, header } from './style.css';
+import logoUrl from 'assets/image/wgt/yonyou_logo.svg';
+
+const { changeUserInfoDisplay } = homeActions;
+
+@connect(
+  mapStateToProps(),
+  {
+    changeUserInfoDisplay,
+  }
+)
+class HeaderPage extends Component {
+  getLeftContent() {
+    return (<img src={logoUrl} className={logoImg}/>);
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      window.scrollTo(0, 1)
+    },0);
+  }
+  render() {
+    const {
+      changeUserInfoDisplay,
+      list,
+    } = this.props;
+    return (
+      <div className={header}>
+        <Header
+          onLeftClick={ changeUserInfoDisplay }
+          leftContent={this.getLeftContent()}
+          iconName={'wode'} >
+          <span>首页编辑</span>
+        </Header>
+        {
+          list.length > 1 ? (
+            <Navbar
+              items={list}
+              offset={-80}
+              duration={500}
+              delay={0} />
+          ) : null
+        }
+      </div>
+    );
+  }
+}
+
+
+export default HeaderPage;
