053bb067585c41e4d4d76e8f1a3bc8f615185797
wangshih@yonyou.com <wangshih@yonyou.com>
2017-12-15 17:02:32 +0800

    最近使用、服务、样式

--- src/workbench/pc/store/root/api.js
+++ src/workbench/pc/store/root/api.js
@@ -2,3 +2,5 @@ import { get } from '@u';
 
 export const getServiceList = () => get('/application/getAllApps');
 export const getMessage = () => get('/getMessage');
+export const getLatestAccessList = () => get('/getLatestAccess');
+export const getPromotionServiceList = () => get('/getPromotionService');
