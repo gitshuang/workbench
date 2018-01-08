72c1777bec76a8bbedc40f3cabdeb9bc934f2847
yangchch6 <ycc54140127@163.com>
2018-01-08 09:59:22 +0800

    全部应用接口

--- src/workbench/pc/store/root/application/api.js
+++ src/workbench/pc/store/root/application/api.js
@@ -1,9 +1,6 @@
 import { get } from '@u';
 
-export const getAllApplicationList = (appName) => get('/serve/getAllAppsGroupByLabels?appName='+appName);
+export const getAllApplicationList = (appName) => get('/application/getAllAppsbyLabelGroup?appName='+appName);
 
 export const getAllApps = () => get('/application/getAllApps');
 
-// export function getAllApps(){
-//   return get('/application/getAllApps');
-// }
