d4cb9f6a5f93ff27119a1245db8ed5d896636383
wangshih@yonyou.com <wangshih@yonyou.com>
2018-01-08 13:50:54 +0800

    匹配后台修改的文件名称

--- config/proxy.js
+++ config/proxy.js
@@ -4,7 +4,7 @@ var api = [
   '/service/getAllServicesGroupByLabels',//废弃
   '/service/getAllServicesByLabelGroup',
   '/application/getAllAppsbyLabelGroup',
-  '/application/getAllApps',
+  '/application/getShortcutApps',
   '/desktop/getdeskTop',
   '/service/getServiceInfoByServiceCode',
   '/service/getServiceInfoByAppCode',
--- src/workbench/pc/store/root/application/api.js
+++ src/workbench/pc/store/root/application/api.js
@@ -1,6 +1,3 @@
 import { get } from '@u';
 
 export const getAllApplicationList = (appName) => get('/application/getAllAppsbyLabelGroup?appName='+appName);
-
-export const getAllApps = () => get('/application/getAllApps');
-
