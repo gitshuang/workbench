49295992dd7b14dfbc5bf737897495d8aeb36117
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-01-12 18:48:15 +0800

    bugfix
    * 修改导航默认选中项

--- src/workbench/pc/components/scrollNav/navs.js
+++ src/workbench/pc/components/scrollNav/navs.js
@@ -9,64 +9,15 @@ import {
 } from './style.css';
 
 class Navs extends Component{
-
-  constructor(props) {
-    super(props);
-
-    Events.scrollEvent.register('begin', function(to, element) {
-      console.log("begin", arguments);
-    });
-
-    Events.scrollEvent.register('end', function(to, element) {
-      console.log("end", arguments);
-      localStorage.setItem("nav",arguments[0]);
-    });
-
-    scrollSpy.update();
-
-  }
-
   componentDidMount() {
-    console.log("====componentDidMount=====" );
     setTimeout(() => {
-      console.log("======setTimeout===" );
-      let lis = document.getElementById("nav_links").getElementsByTagName("a");
-      let default_class = "";
-      let b = false;
-      for(var i=0;i<lis.length;i++){
-        console.log(" lis[i].getAttribute : " + lis[i].getAttribute("class"));
-        b = this.isContains(lis[i].getAttribute("class"));
-        if(!b){
-          default_class = lis[i].getAttribute("class");
-        }
-        lis[i].setAttribute("class",default_class);
+      const firstA = this.refs.list.querySelector('li a');
+      if (firstA) {
+        let newClass = firstA.getAttribute("class");
+        newClass += ` ${activeLink}`;
+        firstA.setAttribute("class", newClass);
       }
-      for(var j=0;j<lis.length;j++){
-        if(lis[j].getAttribute("id") == localStorage.getItem("nav")){
-          lis[j].setAttribute("class",default_class + " active_link")
-        }
-      }
-      // let newClass = lis[0].getAttribute("class");
-      // newClass += " active_link";
-      // console.log("lis[0] ",lis[0]);
-      // b?"":lis[0].setAttribute("class",newClass);
     },0);
-  }
- 
-  isContains(str) {
-    let regx1 = (/active_link/);
-    return str.match(regx1)?true:false;
-  }
-
-  handleSetActive=(e,id)=>{
-    let lis = document.getElementById("nav_links").getElementsByTagName("a");
-    for(var i=0;i<lis.length;i++){
-      let _class = lis[i].getAttribute("class");
-      lis[i].setAttribute("class", _class.replace((/active_link/g)," "));
-      if(lis[i].id == id){
-        lis[i].setAttribute("class", _class+=" active_link");
-      }
-    }
   }
 
   render(){
@@ -79,14 +30,14 @@ class Navs extends Component{
       } = this.props;
     return (
       <div className={cover}>
-        <ul className={navs} id="nav_links">
+        <ul className={navs} ref='list'>
           {
             items = items.map(({ label, target }, i) => (
               <li key={i}>
                 <Link
                   className={link}
                   style={{color:color}}
-                  activeClass="active_link"
+                  activeClass={activeLink}
                   to={target}
                   spy={true}
                   smooth={true}
@@ -94,7 +45,6 @@ class Navs extends Component{
                   duration={duration}
                   isDynamic={true}
                   id={target}
-                  onSetActive={(e)=>{this.handleSetActive(e,target)}}
                   delay={delay}>
                   {label}
                 </Link>
