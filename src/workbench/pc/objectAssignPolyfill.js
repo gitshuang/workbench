883b5fcfb9a233baec2a7051024f38ed88acaf5d
wangshih@yonyou.com <wangshih@yonyou.com>
2018-01-19 10:40:54 +0800

    ie

--- src/workbench/pc/objectAssignPolyfill.js
+++ src/workbench/pc/objectAssignPolyfill.js
@@ -1,5 +1,6 @@
 import objectAssign from 'object-assign';
-
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function () { };
 if (typeof Object.assign === 'undefined') {
   Object.assign = objectAssign;
 }
