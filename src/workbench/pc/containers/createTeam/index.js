4fcd40e7b4d8f9c4c96952ea53215087e4ea9ba5
xingychb <xingychb@yonyou.com>
2018-02-01 20:57:21 +0800

    增加打开user侧边栏的方法

--- src/workbench/pc/containers/createTeam/index.js
+++ src/workbench/pc/containers/createTeam/index.js
@@ -5,11 +5,13 @@ import { mapStateToProps } from '@u';
 
 import rootActions from 'store/root/actions';
 import teamActions from 'store/root/team/actions';
+import homeActions from 'store/root/home/actions';
 
 //
 import { FormControl, Checkbox, Button } from 'tinper-bee';
 
 const { requestStart, requestSuccess, requestError } = rootActions;
+const { changeUserInfoDisplay } = homeActions;
 const { uploadApplication, createTeam} = teamActions;
 import {
   wrap,
@@ -28,7 +30,8 @@ import {
     requestSuccess,
     requestError,
     uploadApplication,
-    createTeam
+    createTeam,
+    changeUserInfoDisplay
   }
 )
 
@@ -95,7 +98,7 @@ class CreateTeamContent extends Component {
 
 
   create = () => {
-    const { history, createTeam, requestStart, requestSuccess, requestError } = this.props;
+    const { history, createTeam, requestStart, requestSuccess, requestError, changeUserInfoDisplay } = this.props;
     const { value, backUrl } = this.state;
     if ( !value ){
       alert("请输入团队名称");
@@ -114,6 +117,7 @@ class CreateTeamContent extends Component {
       } 
       requestSuccess();
       history.replace('/');
+      changeUserInfoDisplay();
     });
     
   }
