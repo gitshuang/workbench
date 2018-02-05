ebe8df5ac9419b5d75669bab29914b4dfb71171a
xingychb <xingychb@yonyou.com>
2018-02-05 13:38:14 +0800

    编辑

--- src/workbench/pc/containers/establishContent/index.js
+++ src/workbench/pc/containers/establishContent/index.js
@@ -4,8 +4,13 @@ import { Button } from 'tinper-bee';
 import {
   wrap,
   desc,
-  box
+  box,
+  imageBox,
+  content
 } from './index.css';
+
+import Team from 'assets/image/wgt/team.png';
+import Company from 'assets/image/wgt/company.png';
 
 @withRouter
 class EstablishContent extends Component {
@@ -34,21 +39,48 @@ class EstablishContent extends Component {
     const { userInfo } = this.props;
     return (
       <div className={wrap}>
-        <h5>{userInfo.userName}，欢迎来到用友Diwork</h5>  
+        <h5>Hi {userInfo.userName}，欢迎来到用友Diwork</h5>  
         <p className={desc}>在正式使用之前，您需要先有所属的团队或企业。</p>
         <div className="um-box">
           <div className={box}>
+            <div className={imageBox}>
+              <img src = {Team} />
+            </div>
             <h6>创建团队</h6>
-            <div></div>
-            <div>
-              <Button onClick={this.openTeam}>开始创建</Button>  
+            <div className={content}>
+              <p>创建后你可以使用以下功能：</p>
+              <ul className="clearfix">
+                <li>· 项目管理</li>  
+                <li>· 共享图书</li>  
+                <li>· 文件管理</li>  
+                <li>· 工作圈/投票/话题</li>  
+                <li>· 笔记共享</li>  
+                <li>· 电话会议/视频会议</li>  
+                <li>· 商旅出行</li>  
+              </ul>  
+            </div>
+            <div style={{textAlign:"center"}}>
+              <button onClick={this.openTeam}>开始创建</button>  
             </div>
           </div>
           <div className={box}>
+          <div className={imageBox}>
+              <img src = {Company} />
+            </div>
             <h6>创建企业</h6>
-            <div></div>
-            <div>
-              <Button>开始创建</Button>  
+            <div className={content}>
+              <p>开通企业你可以使用以下功能：</p>
+              <ul className="clearfix">
+                <li>· 企业组织管理</li>  
+                <li>· 工资条</li>  
+                <li>· 入离职</li>  
+                <li>· 自助档案</li>  
+                <li>· 考勤打卡/请假/报表</li>  
+                <li>· 企业公告/新闻</li>  
+              </ul>  
+            </div>
+            <div style={{textAlign:"center"}}>
+              <button>开始创建</button>  
             </div>
           </div>
         </div>
