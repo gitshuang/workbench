5cb6e5e1fb60827db137bf3d206f23724c1c9bae
zhengxc <zhengxc@yonyou.com>
2018-02-05 19:09:55 +0800

    添加正式上线启动脚本

--- config/index.js
+++ config/index.js
@@ -1,10 +1,12 @@
 var build = require('./prod')
 var dev = require('./dev')
 var test = require('./test')
+var formal = require('./formal')
 var configs = {
   build: build,
   dev: dev,
   test: test,
+  formal: formal,
 }
 
 module.exports = function (key) {
