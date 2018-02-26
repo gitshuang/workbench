e61adfe2087369b08d126cd8ac27d6eec491aad2
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-02-26 17:43:05 +0800

    添加应用市场页面

---
+++ src/workbench/pc/pages/market/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { getHost } from '@u';
+import ApplicationWrap from 'containers/applicationWrap';
+import {
+  bg,
+  frameElm,
+} from './style.css';
+
+@withRouter
+class Account extends Component {
+  goBack = () => {
+    this.props.history.go(-1);
+  }
+  render() {
+    return (
+      <ApplicationWrap name='应用市场' brms={[{name: '全部应用'}]} goBack={this.goBack}>
+        <div className={bg+" um-content um-vbox"}>
+          <iframe className={ frameElm } src={'/diwork-market'} />
+        </div>
+      </ApplicationWrap>
+    );
+  }
+}
+
+export default Account;
