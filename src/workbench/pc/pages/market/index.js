28c3785b978032fbcd296fef31e31ea99dd4e93e
xyoooo <yaoxinc@yonyou.com>
2018-03-10 16:08:03 +0800

    修改面包屑操作

--- src/workbench/pc/pages/market/index.js
+++ src/workbench/pc/pages/market/index.js
@@ -5,7 +5,7 @@ import { mapStateToProps } from '@u';
 import { getHost } from '@u';
 import ApplicationWrap from 'containers/applicationWrap';
 import workActions from 'store/root/work/actions';
-const {addBrm} = workActions;
+const {addBrm,popBrm} = workActions;
 import {
   bg,
   frameElm,
@@ -15,37 +15,56 @@ import {
 @connect(
   mapStateToProps(
       'brm',
+      'brmBackIndex',
       {
           namespace: 'work',
       },
   ),
   {
-    addBrm
+    addBrm,
+    popBrm
   }
 )
 class Account extends Component {
 
   componentDidMount(){
-    this.props.addBrm({name:'全部应用',url:'/market'})
-    this.props.addBrm({name:'应用市场',url:'/market'})
+    if(this.props.brm.length >0){
+      this.props.addBrm({name:'应用市场',url:'/market'})
+
+    }else{
+      this.props.addBrm({name:'全部应用',url:'/application'})
+      this.props.addBrm({name:'应用市场',url:'/market'})
+    }
   }
  
+  goBack = (i) => {
+    this.props.popBrm(i);
+    //跳转
+    if(i == -1){
+      this.props.history.go(-1);
+    }else{
+      let stateBrm = this.props.brm;
+      let popBrmName = stateBrm[stateBrm.length-1][i].name;//面包屑一直展示的是brm的最后一个元素
+      let item,backVal;
+      for(let i = 0;i<stateBrm.length ; i++){
+          item=stateBrm[i];
+          if(item[item.length-1].name == popBrmName){
+              backVal = stateBrm.length-1-i;
+              break;
 
-  goBack = () => {
-    this.props.history.go(-1);
-   
-  }
-
-  func = () =>{
-    if(this.props.brm.length ==1){
+          }
+      }
+      this.props.history.go(-backVal);
     }
   }
 
+  
   render() {
     let brmVal = this.props.brm;
-    console.log('1111112222226666渲染之前的brm的是多少呢',brmVal)
+    let brmLastVal = brmVal.length > 0 && brmVal[brmVal.length-1];
+    let brmLastLen = brmVal.length > 0 && brmVal[brmVal.length-1].length;
     return (
-      <ApplicationWrap name={brmVal.length <= 1?'应用市场':brmVal[brmVal.length-1].name} brms={brmVal.length == 0?[{name:'全部应用'}]:brmVal.slice(0,brmVal.length-1)} goBack={this.goBack}>
+      <ApplicationWrap name={brmVal.length>0 ? brmLastVal[brmLastLen-1].name:'12'} brms={brmVal.length>1?brmVal[brmVal.length-2]:[{name:''}]} goBack={this.goBack}>
         <div className={bg+" um-content um-vbox"}>
           {/* <iframe className={frameElm} src={'http://localhost:3005'} /> */}
           <iframe className={frameElm} src={'/diwork-market/appMarket'} />
