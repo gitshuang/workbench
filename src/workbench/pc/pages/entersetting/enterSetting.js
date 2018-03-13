f8d8f549510022b1fdd7cea2d1efdbfc849eac85
wangshi <wangshih@yonyou.com>
2018-03-13 13:32:33 +0800

    bugfix

--- src/workbench/pc/pages/entersetting/enterSetting.js
+++ src/workbench/pc/pages/entersetting/enterSetting.js
@@ -325,7 +325,7 @@ class CreateEnter extends Component {
                 </FormItem>
               ) : <div />
             }
-            <FormItem showMast={false} labelName={<span>地址<font color='red'>&nbsp;*&nbsp;</font></span>} isRequire={true} valuePropsName='value' errorMessage="请输入企业地址" method="blur" inline={true}>
+            <FormItem showMast={false} isRequire={true} valuePropsName='value' errorMessage="请输入企业地址" method="blur" inline={true}>
               <FormControl name="tenantAddress" value={newTenantAddress?newTenantAddress:tenantAddress} onChange={(e)=>{this.inputOnChange(e,"tenantAddress")}} placeholder="最多60个字符" />
             </FormItem>
 
