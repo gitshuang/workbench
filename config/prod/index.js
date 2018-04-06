c29716677f1a0a5b6589ae8b9aff923aef20de81
zhengxingcheng <xingcheng.zheng@gmail.com>
2018-04-06 18:28:50 +0800

    bugfix
    * 修复稳定测试环境链接错误问题

--- config/prod/index.js
+++ config/prod/index.js
@@ -2,7 +2,7 @@ var webpackConfig = require('./webpack')
 
 var config = {
   env: '"development"',
-  host: '"/"',
+  host: '""',
   assetsSubDirectory: 'static',
   // assetsPublicPath: 'https://cdn.yonyoucloud.com/',
   assetsPublicPath: 'http://workbench.yyuap.com/fe/',
