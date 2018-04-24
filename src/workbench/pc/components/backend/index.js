14e99f0326447a458b206451bade21acfce8ec73
xingychb <xingychb@yonyou.com>
2018-04-24 21:03:45 +0800

    组件eslint 更改

--- src/workbench/pc/components/backend/index.js
+++ src/workbench/pc/components/backend/index.js
@@ -1,18 +1,17 @@
-import MouseBackend from './MouseBackend'
 import HTML5BackendGroup from 'react-dnd-html5-backend';
+import MouseBackend from './MouseBackend';
+
 // import MouseBackEnd from 'react-dnd-mouse-backend'
 
-
-const createMouseBackend = (manager) => new MouseBackend(manager)
+const createMouseBackend = manager => new MouseBackend(manager);
 
 const judgeIfSupH5 = () => {
-    if (window.hasOwnProperty('applicationCache') && window.hasOwnProperty('ondrag')) {
-      return true;
-    } else {
-      return false;
-    }  
-}
+  if (Object.prototype.hasOwnProperty.call(window, 'applicationCache') && Object.prototype.hasOwnProperty.call(window, 'ondrag')) {
+    return true;
+  }
+  return false;
+};
 
-var judgedBackend = judgeIfSupH5()?HTML5BackendGroup:createMouseBackend;
+const judgedBackend = judgeIfSupH5() ? HTML5BackendGroup : createMouseBackend;
 
 export default judgedBackend;
--- src/workbench/pc/components/widget/normal.js
+++ src/workbench/pc/components/widget/normal.js
@@ -1,141 +1,145 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from 'bee/loading';
-import { getContext } from '@u';
 import WidgetTool from 'public/componentTools';
 import {
   widgetItem,
   title,
-  title_left,
-  title_right,
-  content,
+  titleRight,
 } from './style.css';
 
 const widgetStyle = [
   // 小
   {
-    width: 176
+    width: 176,
   },
   // 中
   {
-    width: 360
+    width: 360,
   },
   // 大
   {
     width: 360,
-    height: 360
-  }
+    height: 360,
+  },
 ];
 
-  function getFetchIe9(url, options = {}){
-    if (window.XDomainRequest) {
-      // if(url.indexOf("https") != -1){
-      //   url = url.replace(/https/, "http");
-      // }
-      let fh = url.indexOf("?") == -1?"?":"&";
-      url += fh+"tm="+new Date().getTime();
-      return new Promise((resolve, reject) => {
-        const method = options.method || 'get';
-        const timeout = options.timeout || 30000;
-        const XDR = new XDomainRequest();
-        XDR.open(method, url);
-        XDR.timeout = timeout;
-        XDR.onload = () => {
-          try { 
-            return resolve(XDR.responseText);
-          } catch (e) {
-            reject(e);
-          }
-          return reject({});
-        };
-        XDR.onprogress = () => {};
-        XDR.ontimeout = () => reject('XDomainRequest timeout');
-        XDR.onerror = () => reject('XDomainRequest error');
-        setTimeout(() => {
-          XDR.send();
-        }, 0);
-      });
-    } else {
-    }
-  }
-
-  function getResultFetch(_this,text, callback) {
-    _this.tool = new WidgetTool(_this.props.data.widgetId);
-    try {
-      const fn = new Function(
-        'React',
-        'widgetInstance',
-        'widgetTool',
-        'widgetContext',
-        'return ' + text,
-      );
-      const result = fn(
-        React,
-        _this.props.data,
-        _this.tool,
-        getContext(),
-      );
-      callback(result);
-    } catch (e) {
-      console.log(e);
-    } 
-  }
-
-
-  function getData(url, callback) {
-    let browser=navigator.appName;
-    let b_version=navigator.appVersion;
-    let _resultPremise = null;
-    if(browser=="Microsoft Internet Explorer" && b_version.match(/9./i)=="9.")
-    {
-      getFetchIe9(url,{method:"get",timeout:3000}).then((text) => {
-        getResultFetch(this,text, callback);
-      });
-    }else{
-      fetch(url, {
-        method: 'GET',
-        cache: 'no-cache',
-        headers: {
-          'Content-Type': 'text/plain;charset=UTF-8',
-        },
-      }).then((response) => {
-        if (response.ok) {
-          return response.text().then((text) => {
-            if (text) {
-              getResultFetch(this,text, callback);
-            } else {
-              return Promise.reject(new Error('接口未返回数据'));
-            }
-          });
+function getFetchIe9(url, options = {}) {
+  if (window.XDomainRequest) {
+    // if(url.indexOf("https") != -1){
+    //   url = url.replace(/https/, "http");
+    // }
+    const fh = url.indexOf('?') === -1 ? '?' : '&';
+    // url += fh + "tm=" + new Date().getTime();
+    const time = new Date().getTime();
+    url = `${fh}tm=${time}`;
+    return new Promise((resolve, reject) => {
+      const method = options.method || 'get';
+      const timeout = options.timeout || 30000;
+      const XDR = new XDomainRequest();
+      XDR.open(method, url);
+      XDR.timeout = timeout;
+      XDR.onload = () => {
+        try {
+          return resolve(XDR.responseText);
+        } catch (e) {
+          reject(e);
         }
-        return Promise.reject(new Error('请求失败'));
-      });
-    }
+        return reject();
+      };
+      XDR.onprogress = () => { };
+      XDR.ontimeout = () => Promise.reject(new Error('XDomainRequest timeout'));
+      XDR.onerror = () => Promise.reject(new Error('XDomainRequest error'));
+      setTimeout(() => {
+        XDR.send();
+      }, 0);
+    });
   }
+  return false;
+}
+
+function getResultFetch(that, text, callback) {
+  that.tool = new WidgetTool(that.props.data.widgetId);
+  try {
+    // const fn = new Function(
+    //   'React',
+    //   'widgetInstance',
+    //   'widgetTool',
+    //   'widgetContext',
+    //   'return ' + text,
+    // );
+
+    // const result = fn(
+    //   React,
+    //   that.props.data,
+    //   that.tool,
+    //   getContext(),
+    // );
+    // callback(result);
+    callback(text);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+function getData(url, callback) {
+  const browser = navigator.appName;
+  const bVersion = navigator.appVersion;
+  if (browser === 'Microsoft Internet Explorer' && bVersion.match(/9./i) === '9.') {
+    getFetchIe9(url, { method: 'get', timeout: 3000 }).then((text) => {
+      getResultFetch(this, text, callback);
+    });
+  } else {
+    fetch(url, {
+      method: 'GET',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'text/plain;charset=UTF-8',
+      },
+    }).then((response) => {
+      if (response.ok) {
+        return response.text().then((text) => {
+          if (text) {
+            getResultFetch(this, text, callback);
+          } else {
+            return Promise.reject(new Error('接口未返回数据'));
+          }
+          return false;
+        });
+      }
+      return Promise.reject(new Error('请求失败'));
+    });
+  }
+}
 
 
 class WidgetItem extends Component {
   static propTypes = {
-    data: PropTypes.any.isRequired,
+    data: PropTypes.shape({
+      jsurl: PropTypes.string,
+    }),
   }
+  static defaultProps = {
+    data: {},
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       loaded: false,
       widget: null,
-    }
+    };
   }
   componentWillMount() {
-    const { data:{ jsurl } } = this.props;
+    const { data: { jsurl } } = this.props;
     if (jsurl) {
       getData.call(this, jsurl, (result) => {
         this.setState({
           loaded: true,
           widget: result.default ? result.default : result,
         });
-      })
+      });
     }
   }
   componentWillUnmount() {
@@ -147,18 +151,16 @@ class WidgetItem extends Component {
     const {
       data: {
         background,
-        widgetId: id,
         size,
         widgetName: name,
-        icon,
-      }
+      },
     } = this.props;
     const { widget: Widget, loaded } = this.state;
     let contentElm;
     if (loaded) {
-      contentElm = (<Widget/>);
+      contentElm = (<Widget />);
     } else {
-      contentElm = (<Loading container={this} show={true} />);
+      contentElm = (<Loading container={this} show />);
     }
 
     const style = {
@@ -171,7 +173,7 @@ class WidgetItem extends Component {
     return (
       <li className={widgetItem} style={style} >
         <div className={title}>
-          <div className={title_right}>{name}</div>
+          <div className={titleRight}>{name}</div>
         </div>
         {contentElm}
       </li>
