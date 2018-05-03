27dbb902d4f157735359889921104cc4ebaaa69d
xingychb <xingychb@yonyou.com>
2018-05-03 14:40:25 +0800

    注释代码  清除代码quick namespace

--- src/workbench/pc/containers/quickService/index.js
+++ src/workbench/pc/containers/quickService/index.js
@@ -14,9 +14,6 @@ const { changeQuickServiceHidden } = actions;
   mapStateToProps(
     'serviceList',
     'quickServiceDisplay',
-    {
-      "namespace":"root"
-    }
   ),
   {
     changeQuickServiceHidden,
