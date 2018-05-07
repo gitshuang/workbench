29ad89fa0293a283464415943a9146be4add6a68
xingychb <xingychb@yonyou.com>
2018-05-07 11:25:52 +0800

    eslint 页面   修复serviceClass wraning

--- src/workbench/pc/pages/home/headerPage.js
+++ src/workbench/pc/pages/home/headerPage.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { mapStateToProps, GetQueryString } from '@u';
-import rootActions from 'store/root/actions';
+import PropTypes from 'prop-types';
+import { mapStateToProps } from '@u';
+
 /*   actions   */
+import rootActions from 'store/root/actions';
 import homeActions from 'store/root/home/actions';
 import Icon from 'pub-comp/icon';
 import Header from 'containers/header';
 import Navbar from 'components/scrollNav';
-import { logoImg, header ,imgInner,all_btn,btn_disable,logo_title,title_drop} from './style.css';
-import logoUrl from 'assets/image/wgt/yonyou_logo.svg';
 import DropdownButton from 'components/dropdown';
-const { changeUserInfoDisplay,hideUserInfoDisplay, getUserInfo, changeRequestDisplay ,getSearchEnterOrTeam} = homeActions;
+import { header, imgInner, allBtn, btnDisable } from './style.css';
+
+
+const {
+  changeUserInfoDisplay,
+  hideUserInfoDisplay, getUserInfo, changeRequestDisplay, getSearchEnterOrTeam,
+} = homeActions;
 
 const {
   requestStart,
   requestSuccess,
-  requestError
+  requestError,
 } = rootActions;
 
 @connect(
@@ -25,7 +31,7 @@ const {
     'userInfo',
     {
       namespace: 'home',
-    }
+    },
   ),
   {
     getSearchEnterOrTeam,
@@ -35,106 +41,83 @@ const {
     getUserInfo,
     requestStart,
     requestSuccess,
-    requestError
-  }
+    requestError,
+  },
 )
 class HeaderPage extends Component {
-
-  constructor(props){
+  static propTypes = {
+    getSearchEnterOrTeam: PropTypes.func,
+    changeUserInfoDisplay: PropTypes.func,
+    hideUserInfoDisplay: PropTypes.func,
+    changeRequestDisplay: PropTypes.func,
+    getUserInfo: PropTypes.func,
+    requestStart: PropTypes.func,
+    requestSuccess: PropTypes.func,
+    requestError: PropTypes.func,
+    searchEnterOrTeamList: PropTypes.array,
+    userInfo: PropTypes.shape({
+      name: PropTypes.string,
+      company: PropTypes.string,
+      userAvator: PropTypes.string,
+    }),
+    userInfoDisplay: PropTypes.bool,
+    list: PropTypes.arrayOf(PropTypes.object),
+    headerData: PropTypes.shape({
+      background: PropTypes.string,
+      title: PropTypes.string,
+      titleStyle: PropTypes.string,
+      color: PropTypes.string,
+    }),
+  };
+  static defaultProps = {
+    getSearchEnterOrTeam: () => {},
+    changeUserInfoDisplay: () => {},
+    hideUserInfoDisplay: () => {},
+    changeRequestDisplay: () => {},
+    getUserInfo: () => {},
+    requestStart: () => {},
+    requestSuccess: () => {},
+    requestError: () => {},
+    searchEnterOrTeamList: [],
+    userInfo: {},
+    userInfoDisplay: false,
+    list: [],
+    headerData: {},
+  };
+  constructor(props) {
     super(props);
     this.state = {
-      allBtn:false, //默认显示一行tab
-      btnShow:false,
-      allowTenants:[],
-    }
+      allBtn: false, // 默认显示一行tab
+      btnShow: false,
+      allowTenants: [],
+    };
   }
 
-  componentWillReceiveProps(nextProps){
-    if(this.props.searchEnterOrTeamList != nextProps.searchEnterOrTeamList){
-      this.setState({
-        allowTenants:nextProps.searchEnterOrTeamList
-      })
-    }
-  }
-
-  changeTenant(tenantId){
-    const {
-      location: {
-        origin,
-        pathname,
-        hash,
-      },
-    } = window;
-    window.location.replace(
-      `${origin?origin:''}${pathname?pathname:''}?tenantId=${tenantId}&switch=true${hash}`,
-    );
-  }
-
-  closeFun = ()=>{
-    const {
-      changeUserInfoDisplay,
-      hideUserInfoDisplay,
-      userInfoDisplay
-    } = this.props;
-    if(userInfoDisplay){
-      hideUserInfoDisplay();
-    }
-  }
-
-  openMenu=()=>{
-    const {getSearchEnterOrTeam} = this.props;
-    getSearchEnterOrTeam();
-  }
-
-  getLeftContent() {
-    const {
-      userInfo: {
-        logo,
-        company,
-      },
-    } = this.props;
-    const {allowTenants} = this.state;
-    return (<DropdownButton
-    getPopupContainer={() => document.getElementById("home_header")}
-    openMenu={this.openMenu}
-    closeFun={this.closeFun}
-    label={company} type="home" dataItem={
-      allowTenants.map(({
-        tenantId: name,
-        tenantName: value,
-        team:type
-      }) => {
-        return {
-          name,
-          value,
-          type,
-          fun: this.changeTenant,
-        };
-      })
-    } />);
-  }
   componentDidMount() {
     this.getUserInfo();
-    const { changeUserInfoDisplay,changeRequestDisplay } = this.props; 
+    const { changeUserInfoDisplay, changeRequestDisplay } = this.props;
     // 判断是否localstorage中包含这个值
-    if(localStorage.getItem("create")) {
+    if (localStorage.getItem('create')) {
       changeUserInfoDisplay();
       changeRequestDisplay();
-      localStorage.removeItem("create");
+      localStorage.removeItem('create');
     }
-      // let ul = document.getElementById("nav_ul");
-      // let b = ul.scrollWidth > ul.clientWidth?true:false;
-      // this.setState({
-      //   btnShow:b
-      // })
-
-    // setTimeout(() => {
-    //   window.scrollTo(0, 1);
-    // },0);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.searchEnterOrTeamList !== nextProps.searchEnterOrTeamList) {
+      this.setState({
+        allowTenants: nextProps.searchEnterOrTeamList,
+      });
+    }
+  }
+
+  onLeftTitleClick=() => {}
+
   getUserInfo() {
-    const { userInfo: { name }, getUserInfo ,requestStart, requestSuccess, requestError} = this.props;
+    const {
+      userInfo: { name }, getUserInfo, requestStart, requestSuccess, requestError,
+    } = this.props;
     if (!name) {
       requestStart();
       getUserInfo().then(({ error, payload }) => {
@@ -147,21 +130,73 @@ class HeaderPage extends Component {
     }
   }
 
-  allBtnOnclick=()=>{ 
-    this.setState({
-      allBtn:this.state.allBtn?false:true
-    })
+  getLeftContent() {
+    const {
+      userInfo: {
+        company,
+      },
+    } = this.props;
+    const { allowTenants } = this.state;
+    return (<DropdownButton
+      getPopupContainer={() => document.getElementById('home_header')}
+      openMenu={this.openMenu}
+      closeFun={this.closeFun}
+      label={company}
+      type="home"
+      dataItem={
+      allowTenants.map(({
+        tenantId: name,
+        tenantName: value,
+        team: type,
+      }) => ({
+          name,
+          value,
+          type,
+          fun: this.changeTenant,
+        }))
+    }
+    />);
   }
 
-  btnShowFn = (btnShow)=>{
+  changeTenant = (tenantId) => {
+    const {
+      location: {
+        origin,
+        pathname,
+        hash,
+      },
+    } = window;
+    window.location.replace(`${origin || ''}${pathname || ''}?tenantId=${tenantId}&switch=true${hash}`);
+  }
+
+  closeFun = () => {
+    const {
+      hideUserInfoDisplay,
+      userInfoDisplay,
+    } = this.props;
+    if (userInfoDisplay) {
+      hideUserInfoDisplay();
+    }
+  }
+
+  openMenu=() => {
+    const { getSearchEnterOrTeam } = this.props;
+    getSearchEnterOrTeam();
+  }
+
+
+  allBtnOnclick=() => {
     this.setState({
-      btnShow
+      allBtn: !this.state.allBtn,
     });
   }
 
-  onLeftTitleClick=(e)=>{
-
+  btnShowFn = (btnShow) => {
+    this.setState({
+      btnShow,
+    });
   }
+
 
   render() {
     const {
@@ -169,33 +204,33 @@ class HeaderPage extends Component {
       hideUserInfoDisplay,
       userInfoDisplay,
       list,
-      headerData
+      headerData,
     } = this.props;
 
-    let img = this.props.userInfo.userAvator;
+    const img = this.props.userInfo.userAvator;
     let imgIcon = null;
-    //let class2 = headerData && headerData.className;
-    let background = headerData && headerData.background && JSON.parse(headerData.background);
-    let titleContent = headerData && headerData.title;
-    let titleStyle = headerData && headerData.titleStyle && JSON.parse(headerData.titleStyle);
-    let color = headerData && headerData.color;
-    if(img){
-      imgIcon = <img src={img} className={imgInner} />
-    }else{
-      imgIcon =  <Icon type="staff" />;
-    } 
-    const _btnShow = this.state.btnShow?null:btn_disable;
+    // let class2 = headerData && headerData.className;
+    const background = headerData && headerData.background && JSON.parse(headerData.background);
+    const titleContent = headerData && headerData.title;
+    const titleStyle = headerData && headerData.titleStyle && JSON.parse(headerData.titleStyle);
+    const color = headerData && headerData.color;
+    if (img) {
+      imgIcon = <img alt="" src={img} className={imgInner} />;
+    } else {
+      imgIcon = <Icon type="staff" />;
+    }
+    const BtnShow = this.state.btnShow ? null : btnDisable;
 
     return (
       <div className={`${header}`} style={background} id="home_header">
         <Header
-          onLeftClick={ userInfoDisplay?hideUserInfoDisplay:changeUserInfoDisplay }
+          onLeftClick={userInfoDisplay ? hideUserInfoDisplay : changeUserInfoDisplay}
           onLeftTitleClick={this.onLeftTitleClick}
           leftContent={this.getLeftContent()}
           iconName={imgIcon}
           color={color}
         >
-          <span style={titleStyle}>{titleContent ? titleContent: "首页"}</span>
+          <span style={titleStyle}>{titleContent || '首页'}</span>
         </Header>
         {
           list.length > 1 ? (
@@ -210,14 +245,17 @@ class HeaderPage extends Component {
             />
           ) : null
         }
-        <div className={`${all_btn} ${_btnShow}`} onClick={this.allBtnOnclick}>
-          {this.state.allBtn?"收起":"显示全部"}
-          <Icon type={this.state.allBtn?"upward":"pull-down"} />
+        <div
+          className={`${allBtn} ${BtnShow}`}
+          onClick={this.allBtnOnclick}
+          onKeyDown={this.allBtnOnclick}
+          role="presentation"
+        >
+          {this.state.allBtn ? '收起' : '显示全部'}
+          <Icon type={this.state.allBtn ? 'upward' : 'pull-down'} />
         </div>
       </div>
     );
   }
 }
-
-
 export default HeaderPage;
