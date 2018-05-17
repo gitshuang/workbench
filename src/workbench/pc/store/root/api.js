f2aee9ccae39c6ccb53c96ee57a78ce969aeff9e
xingychb <xingychb@yonyou.com>
2018-05-17 20:35:06 +0800

    修改地址

--- src/workbench/pc/store/root/api.js
+++ src/workbench/pc/store/root/api.js
@@ -6,4 +6,4 @@ export const getLatestAccessList = () => get('/getLatestAccess');
 export const getPromotionServiceList = () => get('/getPromotionService');
 export const uploadApplication = data => postFileCros('https://pubapi.yonyoucloud.com/file/upload/oss/workbench-image-path-applicationIcon', data);
 export const getPoll = () => get('/diwork-heartbeat/heartbeat/checkSession');
-export const getPortal = () => get(' /portalCtrl/getPortal');
+export const getPortal = () => get('/manager/portalCtrl/getPortal');
