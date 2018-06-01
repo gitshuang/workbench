b2662baf66efbafd0f51a4cdcbeeceb24303a922
xingychb <xingychb@yonyou.com>
2018-06-01 10:30:16 +0800

    去掉userinfoDisplay  和    quickdispaly

--- src/workbench/pc/pages/home/headerPage.js
+++ src/workbench/pc/pages/home/headerPage.js
@@ -19,8 +19,7 @@ import { header, allBtn, btnDisable } from './style.css';
 
 
 const {
-  changeUserInfoDisplay,
-  hideUserInfoDisplay, getUserInfo,
+  getUserInfo,
   changeRequestDisplay,
   getSearchEnterOrTeam,
   getWorkList,
@@ -39,7 +38,6 @@ const { openExitModal } = teamconfigActions;
 @connect(
   mapStateToProps(
     'searchEnterOrTeamList',
-    'userInfoDisplay',
     'userInfo',
     {
       namespace: 'home',
@@ -47,8 +45,6 @@ const { openExitModal } = teamconfigActions;
   ),
   {
     getSearchEnterOrTeam,
-    changeUserInfoDisplay,
-    hideUserInfoDisplay,
     changeRequestDisplay,
     getUserInfo,
     requestStart,
@@ -62,8 +58,6 @@ const { openExitModal } = teamconfigActions;
 class HeaderPage extends Component {
   static propTypes = {
     getSearchEnterOrTeam: PropTypes.func,
-    changeUserInfoDisplay: PropTypes.func,
-    hideUserInfoDisplay: PropTypes.func,
     changeRequestDisplay: PropTypes.func,
     getUserInfo: PropTypes.func,
     requestStart: PropTypes.func,
@@ -75,7 +69,6 @@ class HeaderPage extends Component {
       company: PropTypes.string,
       userAvator: PropTypes.string,
     }),
-    userInfoDisplay: PropTypes.bool,
     list: PropTypes.arrayOf(PropTypes.object),
     headerData: PropTypes.shape({
       background: PropTypes.string,
@@ -86,8 +79,6 @@ class HeaderPage extends Component {
   };
   static defaultProps = {
     getSearchEnterOrTeam: () => {},
-    changeUserInfoDisplay: () => {},
-    hideUserInfoDisplay: () => {},
     changeRequestDisplay: () => {},
     getUserInfo: () => {},
     requestStart: () => {},
@@ -95,7 +86,6 @@ class HeaderPage extends Component {
     requestError: () => {},
     searchEnterOrTeamList: [],
     userInfo: {},
-    userInfoDisplay: false,
     list: [],
     headerData: {},
   };
@@ -113,10 +103,9 @@ class HeaderPage extends Component {
   }
 
   componentDidMount() {
-    const { changeUserInfoDisplay, changeRequestDisplay } = this.props;
+    const {  changeRequestDisplay } = this.props;
     // 判断是否localstorage中包含这个值
     if (localStorage.getItem('create')) {
-      changeUserInfoDisplay();
       changeRequestDisplay();
       localStorage.removeItem('create');
     }
@@ -171,7 +160,7 @@ class HeaderPage extends Component {
         company,
       },
     } = this.props;
-    const { allowTenants } = this.state;
+    const { allowTenants } = this.state;DropdownButton
     return (<DropdownButton
       getPopupContainer={() => document.getElementById('home_header')}
       openMenu={this.openMenu}
@@ -205,13 +194,7 @@ class HeaderPage extends Component {
   }
 
   closeFun = () => {
-    const {
-      hideUserInfoDisplay,
-      userInfoDisplay,
-    } = this.props;
-    if (userInfoDisplay) {
-      hideUserInfoDisplay();
-    }
+    
   }
 
   openMenu = () => {
--- src/workbench/pc/store/root/home/types.js
+++ src/workbench/pc/store/root/home/types.js
@@ -3,10 +3,8 @@ import { createTypes } from '@u';
 export default createTypes(
   'GET_USER_INFO',
   'GET_WORK_LIST',
-  'CHANGE_USER_INFO_DISPLAY',
   'CHANGE_REQUEST_DISPLAY',
   'CLOSE_REQUEST_DISPLAY',
-  'HIDE_USER_INFO_DISPLAY',
   'OPEN_FOLDER',
   'CLOSE_FOLDER',
   'SET_CUT_USER',
