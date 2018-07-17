075f2b3a01a08512a1f2195b8003acf2a1a7dccf
xyoooo <yaoxinc@yonyou.com>
2018-07-17 11:02:36 +0800

    全部应用的不展示desc

--- src/workbench/pc/containers/serviceClassify/goto.js
+++ src/workbench/pc/containers/serviceClassify/goto.js
@@ -23,7 +23,7 @@ class GoTo extends Component {
         <div className={appItemImg}><img src={icon}/></div>
         <div className={appItem_describe}>
           <p className={appItem_tit}>{appName}</p>
-          <p className={appItem_con}>沟通协作一步到位</p>
+          {/* <p className={appItem_con}>沟通协作一步到位</p> */}
         </div>
       </li>
     )
