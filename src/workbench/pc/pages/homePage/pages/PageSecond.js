493cba255a7e906e293b998f7d45a43ec17f12ee
xyoooo <yaoxinc@yonyou.com>
2018-07-27 15:59:14 +0800

    ie9下面可以滚动

--- src/workbench/pc/pages/homePage/pages/PageSecond.js
+++ src/workbench/pc/pages/homePage/pages/PageSecond.js
@@ -9,12 +9,17 @@ import{
          <div className={`${amPage} PageSecond `}>
            <div className={amBg} ></div>
             <div className="mainContent viewTwo">
-             <div className={`oneArtical ${words}`}>
-                <span className="wordsTitle">“我是一位企业家”</span>
-                <span className="wordsCon">经营企业，我不再焦虑</span>
-                <span className="wordsCon">用友云提供了企业所需要的各种数字化服务 </span>
-                <span className="wordsCon">让我能够运筹帷幄，时刻掌握公司的经营情况 </span>
-             </div>
+            {
+                !props.loginModalShow &&(
+                    <div className={`oneArtical ${words}`}>
+                        <span className="wordsTitle">“我是一位企业家”</span>
+                        <span className="wordsCon">经营企业，我不再焦虑</span>
+                        <span className="wordsCon">用友云提供了企业所需要的各种数字化服务 </span>
+                        <span className="wordsCon">让我能够运筹帷幄，时刻掌握公司的经营情况 </span>
+                    </div>
+                )
+            }
+            
             </div>
          </div>   
       )
