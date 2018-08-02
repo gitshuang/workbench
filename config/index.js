74a041ebee10fac55cb460b33b2eab641c4cb322
xyoooo <yaoxinc@yonyou.com>
2018-08-02 10:18:42 +0800

    为workbench-daily添加执行脚本

--- config/index.js
+++ config/index.js
@@ -2,11 +2,13 @@ var build = require('./prod')
 var dev = require('./dev')
 var test = require('./test')
 var formal = require('./formal')
+var daily = require('./daily');
 var configs = {
   build: build,
   dev: dev,
   test: test,
   formal: formal,
+  daily:daily,
 }
 
 module.exports = function (key) {
