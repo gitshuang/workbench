a96f659f7e6cf987a574a5c432003f9335b62d07
xyoooo <yaoxinc@yonyou.com>
2018-08-02 18:10:47 +0800

    footer

--- src/workbench/pc/pages/loginPage/footer/index.js
+++ src/workbench/pc/pages/loginPage/footer/index.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 
 import{
-    FooterConPanel
+    FooterConPanel,
+    FooterBottom
 } from './index.css';
 
 
 class Footer extends Component {
   constructor(props){
     super(props);
-    
+    this.iconCode  = null;//二维码的code
+    this.iconCodeTimeVal = null ; //防抖动
   }
   componentWillMount(){
     
   }
   componentDidMount(){
     
+  }
+  hover =(e) =>{
+    if(!this.iconCodeTimeVal){
+        this.iconCodeTimeVal = 1;
+        this.iconCode.className = 'iconItem codeItem fadeIn ';
+    }
+  }
+  hoverLeave = () =>{
+      setTimeout(() => {
+        this.iconCode.className = 'iconItem codeItem ';
+        this.iconCodeTimeVal = null;
+      }, 2000);
   }
   
   render() {
@@ -25,18 +39,26 @@ class Footer extends Component {
                 <div className="topSection topOne">
                     <span className="title">客户端下载</span>
                     <div className="allIcons">
-                        <span className="iconItem">
+                        <span className="iconItem diff" onMouseEnter={this.hover} onMouseLeave={this.hoverLeave}>
                             <span className="iconInner iconOne"></span>
+                            <span className="iconDesc">移动端</span>
                         </span>
                         <span className="iconItem">
                             <span className="iconInner iconTwo"></span>
+                            <span className="iconDesc">Mac</span>
                         </span>
                         <span className="iconItem">
                             <span className="iconInner iconThree"></span>
+                            <span className="iconDesc">Windows</span>
                         </span>
                         
                         <span className="iconItem">
                             <span className="iconInner iconFour"></span>
+                            <span className="iconDesc">Win xp</span>
+                        </span>
+                        <span className="iconItem codeItem " ref={(ref)=>{this.iconCode = ref;}}>
+                            <span className="iconInner2 iconCode"></span>
+                            <span className="iconDesc">扫描下载客户端</span>
                         </span>
                     </div>
                 </div>
@@ -53,6 +75,10 @@ class Footer extends Component {
                         <span className="con">联系邮箱：esnservice@yonyou.com</span>
                     </div>
                 </div>
+             </div>
+             <div className={FooterBottom}>
+                <span className="police">京公网安备 11010802020548号</span>
+                <span className="company">用友网络科技股份有限公司 © Copyright 2017 京ICP备15057199号</span>
              </div>
         </div>
     );
