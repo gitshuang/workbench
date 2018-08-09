5d4e3bbfc7138a241401b0c5140abcb3e05d8b13
xyoooo <yaoxinc@yonyou.com>
2018-08-09 21:04:50 +0800

    繁体的shell

--- build/build.sh
+++ build/build.sh
@@ -9,4 +9,7 @@ cp -r ../template/download ./zh_CN/download
 rm -rf ./en_US
 cp -r ../en_US ./
 cp -r ../template/download ./en_US/download
+rm -rf ./zh_TW
+cp -r ../zh_TW ./
+cp -r ../template/download ./zh_TW/download
 docker build -t workbench-fe .
