3e9b93a4ed966a6ea0bb179787f2ee21edd517a2
xingychb <xingychb@yonyou.com>
2018-08-15 15:46:52 +0800

    修改个人账户管理的多语

--- src/workbench/pc/pages/account/index.js
+++ src/workbench/pc/pages/account/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { getHost } from '@u';
+import { getHost, getContext } from '@u';
 import ApplicationWrap from 'containers/applicationWrap';
 import IFrame from 'components/iframe_other';
 import { bg } from './style.css';
@@ -8,10 +8,11 @@ import { bg } from './style.css';
 @withRouter
 class Account extends Component {
   render() {
+    const { locale } = getContext();
     return (
       <ApplicationWrap name="帐号管理">
         <div className={`${bg} um-content um-vbox`}>
-          <IFrame title="帐号管理" url={`${getHost('euc')}/diuser`} />
+          <IFrame title="帐号管理" url={`${getHost('euc')}/diuser?locale=${locale}`} />
         </div>
       </ApplicationWrap>
     );
