4524b04face7ea99415ce872492d5cd317037d62
xyoooo <yaoxinc@yonyou.com>
2018-08-22 18:14:04 +0800

    本地服务不需要验证登录

--- src/workbench/pc/pages/index.js
+++ src/workbench/pc/pages/index.js
@@ -92,7 +92,7 @@ class Root extends Component {
     this.state = {
 
     };
-    this.isLogin = window.os_fe_isLogin && window.os_fe_isLogin();
+    this.isLogin = (window.os_fe_isLogin && window.os_fe_isLogin())||((window.location.hostname.indexOf('localhost')>-1) );
     // this.isLogin = true;
   }
   componentWillMount() {
