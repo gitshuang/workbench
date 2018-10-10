7d55b68acaa58b42a2636e6d935ec263a3020525
xingychb <xingychb@yonyou.com>
2018-10-10 14:01:20 +0800

    放到download中

--- build/build.sh
+++ build/build.sh
@@ -3,7 +3,6 @@ mkdir workspace
 rm -rf ./dist
 cp -r ../dist ./
 cp -r ../template/download ./dist/download
-cp -r ./jDiwork.js ./dist/
 rm -rf ./zh_CN
 cp -r ../zh_CN ./
 cp -r ../template/download ./zh_CN/download
