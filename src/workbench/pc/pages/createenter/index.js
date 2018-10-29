8e1b911766f67ec4291443f22263c31cee61a0ea
xingychb <xingychb@yonyou.com>
2018-10-29 14:22:33 +0800

    增加国区电话

--- src/workbench/pc/pages/createenter/index.js
+++ src/workbench/pc/pages/createenter/index.js
@@ -7,7 +7,9 @@ import { mapStateToProps } from '@u';
 import Header from 'containers/header';
 import Breadcrumbs from 'components/breadcrumb';
 
-import EnterContent from 'pub-comp/enterContent';
+import EnterContent from 'components/enterContent';
+
+// import EnterContent from 'pub-comp/enterContent';
 import { uploadApplication } from 'store/root/api';
 import { texts } from 'components/entertext';
 
