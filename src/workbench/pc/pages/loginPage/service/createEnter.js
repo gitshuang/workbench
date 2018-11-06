40fe48237115bca5716e789efc485870d1117518
yaoxinc <yaoxinc@yonyou.com>
2018-11-06 11:05:42 +0800

    city登录修改

--- src/workbench/pc/pages/loginPage/service/createEnter.js
+++ src/workbench/pc/pages/loginPage/service/createEnter.js
@@ -44,19 +44,24 @@ class CreateEnter extends Component {
       linkman: '',
       companyname:'',
       tenantTel:'',
+      defaultValue:{
+        province:'北京',
+        city:'北京',
+      }
     };
-    // this.address = '北京|北京|东城区|';
-    this.province = '北京';
-    this.city = '北京';
   }
-
   onChange = (obj) => {
-    // this.address = obj.province + obj.city;
-    this.province =  obj.province;
-    this.city= obj.city;
+    this.setState({
+        defaultValue:{
+          province:obj.province,
+          city:obj.city,
+        }
+    })
   }
 
-  
+  componentDidMount(){
+    console.log(this.props)
+  }
 
   setOptherData = (obj) => {
     this.state[obj.name] = obj.value;
@@ -200,7 +205,7 @@ class CreateEnter extends Component {
             method="blur"
             inline
           >
-            <CitySelect name="address" onChange={this.onChange} />
+            <CitySelect name="address" onChange={this.onChange} defaultValue={this.state.defaultValue} lang='zh_CN'/>
           </FormItem>
           <FormItem
             showMast={false}
