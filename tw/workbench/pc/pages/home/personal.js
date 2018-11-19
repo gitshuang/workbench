d61627ace36e52e14bc6c80165db4f736b157fa5
yaoxinc <yaoxinc@yonyou.com>
2018-11-19 15:57:04 +0800

    首页也不需要请求接口

--- tw/workbench/pc/pages/home/personal.js
+++ tw/workbench/pc/pages/home/personal.js
@@ -17,7 +17,7 @@ import { openService, openHomePage } from 'public/regMessageTypeHandler';
 
 const { closeRequestDisplay, getUserInfo } = homeActions;
 const { openExitModal } = teamconfigActions;
-const { setCurrent, getAllEnable, getCurrent } = rootActions;
+const { setCurrent } = rootActions;
 @withRouter
 @connect(
   mapStateToProps(
@@ -36,8 +36,8 @@ const { setCurrent, getAllEnable, getCurrent } = rootActions;
     openExitModal,
     getUserInfo,
     setCurrent,
-    getAllEnable,
-    getCurrent,
+    // getAllEnable,
+    // getCurrent,
   },
 )
 class Personals extends Component {
@@ -142,44 +142,35 @@ class Personals extends Component {
       }
       this.setState({
         userInfo: payload,
-      },()=>{
+      }, () => {
         this.getCompanyType();
       });
 
     });
     //新增 添加多语的所有语言
     this.getAllEnableFunc();
-    //获取默认
-    this.getDefaultLang();
   }
-
   getAllEnableFunc = () => {
-    const { getAllEnable } = this.props;
-    getAllEnable().then(({ error, payload }) => {
-      if (error) {
-        return;
-      }
-      let languageListVal = [], item = {}, defaultValue;
-      payload.map((item, index) => {
+    let allLanArr = window.getEnableLangVOs && window.getEnableLangVOs();
+    let languageListVal = [];
+    if (allLanArr && allLanArr.length) {
+      allLanArr.map((item) => {
         item = { value: item.langCode, context: item.dislpayName }
         languageListVal.push(item);
       });
-
       this.setState({
         language: { ...this.state.language, languageList: languageListVal }
+      }, () => {
+        //获取默认
+        this.getDefaultLang();
       })
-    });
+    }
   }
 
   getDefaultLang = () => {
-    const { getCurrent } = this.props;
-    getCurrent().then(({ error, payload }) => {
-      if (error) {
-        return;
-      }
-      this.setState({
-        language: { ...this.state.language, defaultValue: payload.langCode }
-      });
+    let currentLan = window.getCurrentLangCode && window.getCurrentLangCode();;
+    this.setState({
+      language: { ...this.state.language, defaultValue: currentLan ? currentLan : 'zh_CN' }
     });
   }
 
@@ -226,14 +217,14 @@ class Personals extends Component {
       openService('GZTSYS001');
       return false;
     }
-    if(action === "openHomepage"){
+    if (action === "openHomepage") {
       openHomePage({
         userId: userInfo.userId,
         key: 'honor'
       });
       return false;
     }
-    if(action === "openDynamic"){
+    if (action === "openDynamic") {
       openHomePage({
         userId: userInfo.userId,
         key: 'speak'
@@ -261,7 +252,7 @@ class Personals extends Component {
 
     const currData = currType == 0 ? TeamData[0] : TeamData[1];
     personalText.name = currType == 0 ? '企業' : '團隊';
-
+    console.log(2233333, language.defaultValue, language.languageList[0])
     return (
       <div>
         <Personal
