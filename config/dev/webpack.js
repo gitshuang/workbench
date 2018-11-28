e87389dc33e5a1de147c2ffac328bd6454b95dc0
xingychb <xingychb@yonyou.com>
2018-11-28 13:38:09 +0800

    config 优化

--- config/dev/webpack.js
+++ config/dev/webpack.js
@@ -58,7 +58,7 @@ module.exports = function (config) {
               loader: 'css-loader',
               options: {
                 modules: true,
-                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                localIdentName: '[local]--[hash:base64:5]',
                 importLoaders: 1,
                 sourceMap: false,
               },
