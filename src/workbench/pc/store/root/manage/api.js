3213316d8d5ebbaa90b4f9c214017d91aeb58f29
xingychb <xingychb@yonyou.com>
2018-12-05 13:44:32 +0800

    r

--- src/workbench/pc/store/root/manage/api.js
+++ src/workbench/pc/store/root/manage/api.js
@@ -1,6 +1,6 @@
 import { get, post } from '@u';
 
-export const setManageList = list => post('/desktop/update', list);
+export const setManageList = list => post('/desktop/update', list, true);
 export const getManageList = () => get('/desktop/getdeskTop');
 
 export const getAllServicesByLabelGroup = serviceName => get(`/service/getAllServicesByLabelGroup?serviceName=${serviceName}`);
