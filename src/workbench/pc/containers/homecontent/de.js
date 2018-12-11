e8ca7bbf912882bc1eee35742f3d808c85cc9a4a
xingychb <xingychb@yonyou.com>
2018-12-11 20:41:23 +0800

    g

---
+++ src/workbench/pc/containers/homecontent/de.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+//定义 增强功能的函数 把组件入参
+function IronMan(Component) {
+    //定义 包装组件
+     class IronManComponent extends React.Component{
+         constructor(props){
+             super(props)
+         }
+         render(){
+             //把传入的组件的props展开，这样就能把所有的窜见来props 继承过来
+             return(<div>
+                 <Component {...this.props} />
+                 <p>现在我已经是钢铁侠了，准备打败灭霸</p>
+             </div>)
+         }
+     }
+     //返回包装加强后的组件
+     return IronManComponent
+ }
+
+@IronMan
+class De extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+        console.log(this.isTestable)
+    }
+
+
+    render() {
+        return <div style={{height:"40px",background:"red"}} />
+
+    }
+}
+export default De;
