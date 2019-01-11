565a59eff733f954155833c1f1511bd06ec8d196
lihshf <lihshf@yonyou.com>
2019-01-11 15:23:01 +0800

    去掉无用代码

--- src/workbench/pc/pages/manage/manageWidget/index.js
+++ src/workbench/pc/pages/manage/manageWidget/index.js
@@ -13,23 +13,14 @@ import CustomDragLayer from './dragLayer/customDragLayer.js';
 class CreateManageModule extends Component {
   constructor(props) {
     super(props);
-    this.state={}
+    
   }
-  componentDidMount(){
-    window.addEventListener('resize',()=>{
-      const documentElement = document.documentElement||document.body;
-       this.setState({height: documentElement.clientHeight})
-       console.log("resize===================");
-    })
-  }
-  componentWillUnmount(){
-    window.removeEventListener('resize')
-  }
+
   render() {
-    const {height} = this.state;
+   
     return (
       <div className={management}>
-          <Sider height={height?height:"auto"}/>
+          <Sider />
           <Content {...this.props} languagesJSON={languagesJSON}/>
           <CustomDragLayer/>
       </div>
