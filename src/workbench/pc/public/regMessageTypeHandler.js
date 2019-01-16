1b29661388467f3e63068b994f69ed9f869dc902
xingychb <xingychb@yonyou.com>
2019-01-16 16:19:03 +0800

    兼容openWin 方法

--- src/workbench/pc/public/regMessageTypeHandler.js
+++ src/workbench/pc/public/regMessageTypeHandler.js
@@ -24,8 +24,14 @@ const {
 } = rootActions;
 const { getUserInfo } = homeActions;
 const handlers = {
-  openWin() { 
-    
+  openWin(data) {
+    if (typeof data !== 'object') {
+      throw new Error('data is must be a object.');
+    }
+    if (data.id === undefined) {
+      throw new Error('id is required.');
+    }
+    this.props.history.push(`/${data.id}`);
   },
   openService({ serviceCode, data, type, tenantId }) {
     if (tenantId && serviceCode) {
