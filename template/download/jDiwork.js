87aecc8fc9a8674a9ea7e1f5e3f90929355aefd5
xingychb <xingychb@yonyou.com>
2019-01-17 21:37:06 +0800

    123

--- template/download/jDiwork.js
+++ template/download/jDiwork.js
@@ -22,14 +22,9 @@
     'http://workbenchdev.yyuap.com',
     'https://www.diwork.com',
     'https://workbench-daily.yyuap.com',
+    window.location.origin || window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : ''),
   ];
 
-  var hostname = "yyuap.com";
-  if ((window.location.hostname.indexOf(hostname) !== -1) && (originList.indexOf(window.location.origin) < 0)) {
-    document.domain = hostname;
-  }
-
-  originList.push(window.location.origin || window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : ''),);
 
   var getUid = function () {
     return ++uid;
